fbda30a1783b1868ec44a28b7f44ffc475283f9e
Babbit <ambabbit@yahoo.com>
2019-11-30 13:41:58 -0500

    Completed HW 6 for SE320 11/30/19

---
+++ SE 320 Software Construction/Homework 6/hw6_1/Que62.java
@@ -0,0 +1,15 @@
+public class Que62 {
+    public static void main(String[] args) {
+        Thread firsto = new Thread(new Runnable() {
+            public void run() { System.out.println("first"); };
+        });
+        firsto.start();
+        new Thread(new Runnable() {
+            public void run() { System.out.println("second"); };
+        }).start();
+        new Thread(new Runnable() {
+            public void run() { System.out.println("third"); };
+        });
+        // there is a starting issue here
+    }
+}
